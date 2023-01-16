5fc6e0fff99406be9419ff2d48b0d5ad7e7b9858
TheBestPessimist <cristian+gitpush@tbp.land>
2023-01-16 12:40:53 +0200

    Microsoft Feces

---
+++ Miscellaneous/feces.ps1
@@ -0,0 +1,4 @@
+﻿kill -Name 'Teams' -ErrorAction SilentlyContinue
+kill -Name 'Microsoft.AAD.BrokerPlugin' -ErrorAction SilentlyContinue
+cd C:\Users\TheBestPessimist\AppData\Roaming\Microsoft\Teams
+rm -r -fo *
